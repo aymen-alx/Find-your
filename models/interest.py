3123636ca01d0ff685853c6c75996fb8b96835ea
Mr-robot <hackingtest017@gmail.com>
2023-06-01 08:07:53 -0400

    view update

--- models/interest.py
+++
@@ -1,9 +0,0 @@
-from models import db
-from datetime import datetime
-
-
-# Interest model
-class Interest(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
